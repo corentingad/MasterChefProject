558843be5a0ef6a33392d088c4d9fb79a078e635
greenslimes <cyril.gerardot@viacesi.fr>
2018-12-06 11:28:25 +0100

    Add files via upload

---
+++ Metier/Restaurant.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Metier
+{
+    public class Restaurant
+    {
+
+
+        public List<Carre> CarresList { get; set; }
+        //List<Carre> CarresList = new List<Carre>();
+        public List<GroupeClient> FileAttente { get; set; }
+        public MaitreHotel MaitreHotel { get; set; }
+
+        public Restaurant()
+        {
+            FileAttente = new List<GroupeClient>();
+            CarresList = new List<Carre>();
+        }
+
+        public void Tick()
+        {
+            MaitreHotel.Tick();
+            foreach (var carre in CarresList)
+            {
+                carre.CDR.Tick();
+            }
+        }
+
+        public void TickFor(int x)
+        {
+            for (int i = 0; i < x; i++)
+                Tick();
+        }
+
+        public void GroupeClientArrive(GroupeClient groupeClient)
+        {
+            this.FileAttente.Add(groupeClient);
+
+        }
+
+    }
+}
