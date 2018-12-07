db791fbb5a216bf830c20088a074cef1ed90716c
Corentin GADRET <corentin.gadret@viacesi.fr>
2018-12-07 10:56:25 +0100

    Projet up

---
+++ test/Metier/Personnel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Metier
+{
+    public abstract class Personnel
+    {
+        public string Nom { get; set; }
+
+
+        public Personnel(string nom)
+        {
+            Nom = nom;
+        }
+
+        public void Log(string input)
+        {
+            Console.WriteLine("[" + Nom + "]" + input);
+        }
+
+        public abstract void Tick();
+    }
+}
